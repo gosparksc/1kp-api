a65cc6478ea704a670ac38a7d2d59128a1fe361a
Brian Anglin <laxnski@gmail.com>
2015-11-04 15:36:44 -0800

    Adds example config

---
+++ config.example.py
@@ -0,0 +1,8 @@
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:ponder@localhost/onekp'
+# username: postgres, password: ponder, database: onekp
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+AUTHENTICATION_TOKEN = 'abc'
+DEBUG=True
+AWS_BUCKET_NAME='spark-onekp'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
