c1670fe5ac933d3d3bed9ab0a39e445bf7f9451c
Nathan Wallace <techietotoro@gmail.com>
2016-09-26 10:25:43 -0700

    Updates wsgi.py to work with AWS EB

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,6 @@ from app.app import create_app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app = create_app()
+    application = create_app()
+    app = application
     app.run(host='0.0.0.0', port=port, debug=True)
