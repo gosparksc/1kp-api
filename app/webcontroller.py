4f73ae6a037ccfcc58ea16d9668b9534a902d529
Nathan Wallace <techietotoro@gmail.com>
2017-10-21 01:54:01 -0700

    Adds video viewing page and logo

--- app/webcontroller.py
+++ app/webcontroller.py
@@ -7,6 +7,13 @@ web_blueprint = Blueprint('web_blueprint', __name__, template_folder='templates'
 
 @web_blueprint.route('/', methods=['GET'])
 def index():
-    count = Pitch.query.count() - 13 # Temporary to get the accurate count
+    count = Pitch.query.count() - 13  # Temporary to get the accurate count
     pitches = Pitch.query.all()
     return render_template('index.html', count=count, pitches=pitches)
+
+
+@web_blueprint.route('/v/<int:video_id>', methods=['GET'])
+def v(video_id):
+    count = Pitch.query.count() - 13  # Temporary to get the accurate count
+    pitch = Pitch.query.filter_by(id=video_id).first()
+    return render_template('video.html', count=count, pitch=pitch)
