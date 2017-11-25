e884b73b7075dd534625bee4606243002c4a9c32
Nathan Wallace <techietotoro@gmail.com>
2017-11-24 16:21:39 -0800

    Implements category pages for sorting pitches by category

--- app/webcontroller.py
+++ app/webcontroller.py
@@ -4,6 +4,13 @@ from app.models import Pitch
 
 web_blueprint = Blueprint('web_blueprint', __name__, template_folder='templates')
 
+category_to_url_map = {'Arts, Media & Culture': 'arts',
+                       'Community Impact & Education': 'impact-education',
+                       'Environment': 'environment',
+                       'Health & Biotech': 'health-biotech',
+                       'Life Hacks & Other': 'life-hacks',
+                       'Politics': 'politics'}
+
 
 @web_blueprint.route('/', methods=['GET'])
 def index():
@@ -16,4 +23,24 @@ def index():
 def v(video_id):
     count = Pitch.query.count() - 13  # Temporary to get the accurate count
     pitch = Pitch.query.filter_by(id=video_id).first()
-    return render_template('video.html', count=count, pitch=pitch)
+    category_url = category_to_url_map[pitch.pitch_category]
+    return render_template('video.html', count=count, pitch=pitch, category_url=category_url)
+
+
+@web_blueprint.route('/c/<string:category_id>', methods=['GET'])
+def c(category_id):
+    url_to_category_map = {'arts': 'Arts, Media & Culture',
+                           'impact-education': 'Community Impact & Education',
+                           'environment': 'Environment',
+                           'health-biotech': 'Health & Biotech',
+                           'life-hacks': 'Life Hacks & Other',
+                           'politics': 'Politics'}
+
+    category = url_to_category_map.get(category_id)
+    if category is None:
+        return abort(404)
+
+    count = Pitch.query.count() - 13  # Temporary to get the accurate count
+    pitch = Pitch.query.filter_by(pitch_category=category).all()
+    return render_template('index.html', count=count,
+                           pitches=pitch, category=category)
